0d2936257d585521ed3a4ac6a84ee2909a201161
apalala <apalala@gmail.com>
2026-05-14 15:51:30 -0400

    [leftrec] implement analysis using SCC (inspired by pegen)

--- peg/leftrec.go
+++ peg/leftrec.go
@@ -1,11 +1,8 @@
 package peg
 
-import "fmt"
-
-const (
-	stateFirst = iota
-	stateVisiting
-	stateVisited
+import (
+	"fmt"
+	"sort"
 )
 
 func isNullable(exp Model) bool {
@@ -162,6 +159,90 @@ func unboxExp(e Model) Model {
 	}
 }
 
+func tarjanSCC(edges [][]int) [][]int {
+	n := len(edges)
+	index := make([]int, n)
+	lowlink := make([]int, n)
+	onStack := make([]bool, n)
+	stack := make([]int, 0, n)
+	for i := range index {
+		index[i] = -1
+	}
+	var currentIndex int
+	var sccs [][]int
+
+	var strongconnect func(v int)
+	strongconnect = func(v int) {
+		index[v] = currentIndex
+		lowlink[v] = currentIndex
+		currentIndex++
+		stack = append(stack, v)
+		onStack[v] = true
+
+		for _, w := range edges[v] {
+			if index[w] == -1 {
+				strongconnect(w)
+				if lowlink[w] < lowlink[v] {
+					lowlink[v] = lowlink[w]
+				}
+			} else if onStack[w] {
+				if index[w] < lowlink[v] {
+					lowlink[v] = index[w]
+				}
+			}
+		}
+
+		if lowlink[v] == index[v] {
+			var scc []int
+			for {
+				w := stack[len(stack)-1]
+				stack = stack[:len(stack)-1]
+				onStack[w] = false
+				scc = append(scc, w)
+				if w == v {
+					break
+				}
+			}
+			sccs = append(sccs, scc)
+		}
+	}
+
+	for v := 0; v < n; v++ {
+		if index[v] == -1 {
+			strongconnect(v)
+		}
+	}
+	return sccs
+}
+
+func findCyclesInSCC(edges [][]int, scc []int, start int) [][]int {
+	sccSet := make(map[int]bool)
+	for _, id := range scc {
+		sccSet[id] = true
+	}
+
+	var cycles [][]int
+	var dfs func(node int, path []int)
+	dfs = func(node int, path []int) {
+		for _, p := range path {
+			if p == node {
+				cycles = append(cycles, append([]int{}, path...))
+				return
+			}
+		}
+		path = append(path, node)
+		for _, child := range edges[node] {
+			if sccSet[child] {
+				newPath := append([]int{}, path...)
+				dfs(child, newPath)
+			}
+		}
+	}
+
+	dfs(start, []int{})
+	return cycles
+}
+
 func (g *Grammar) markLeftRecursion() {
 	ruleIndex := make(map[*Rule]int)
 	for i, rule := range g.Rules {
@@ -171,40 +252,70 @@ func (g *Grammar) markLeftRecursion() {
 	}
 
 	edges := make([][]int, len(g.Rules))
+	ruleNames := make([]string, len(g.Rules))
 	for i, rule := range g.Rules {
 		edges[i] = callableRuleIDs(rule.Exp, ruleIndex)
+		ruleNames[i] = rule.Name
 	}
 
-	state := make([]int, len(g.Rules))
-	stack := make([]int, 0, len(g.Rules))
+	sccs := tarjanSCC(edges)
 
-	var dfs func(int)
-	dfs = func(ruleID int) {
-		switch state[ruleID] {
-		case stateVisiting, stateVisited:
-			return
-		}
-		state[ruleID] = stateVisiting
-		stack = append(stack, ruleID)
+	for _, scc := range sccs {
+		if len(scc) > 1 {
+			for _, id := range scc {
+				g.Rules[id].IsMemo = false
+			}
 
-		for _, childID := range edges[ruleID] {
-			switch state[childID] {
-			case stateFirst:
-				dfs(childID)
-			case stateVisiting:
-				g.Rules[childID].IsLrec = true
-				g.Rules[childID].IsMemo = false
-				for _, id := range stack {
+			leaders := make(map[int]bool)
+			for _, id := range scc {
+				leaders[id] = true
+			}
+
+			for _, start := range scc {
+				cycles := findCyclesInSCC(edges, scc, start)
+				for _, cycle := range cycles {
+					cycleSet := make(map[int]bool)
+					for _, id := range cycle {
+						cycleSet[id] = true
+					}
+					for id := range leaders {
+						if !cycleSet[id] {
+							delete(leaders, id)
+						}
+					}
+					if len(leaders) == 0 {
+						break
+					}
+				}
+				if len(leaders) == 0 {
+					break
+				}
+			}
+
+			if len(leaders) == 0 {
+				for _, id := range scc {
+					leaders[id] = true
+				}
+			}
+
+			var leaderIDs []int
+			for id := range leaders {
+				leaderIDs = append(leaderIDs, id)
+			}
+			sort.Slice(leaderIDs, func(i, j int) bool {
+				return ruleNames[leaderIDs[i]] < ruleNames[leaderIDs[j]]
+			})
+			g.Rules[leaderIDs[0]].IsLrec = true
+
+		} else if len(scc) == 1 {
+			id := scc[0]
+			for _, child := range edges[id] {
+				if child == id {
+					g.Rules[id].IsLrec = true
 					g.Rules[id].IsMemo = false
+					break
 				}
 			}
 		}
-
-		stack = stack[:len(stack)-1]
-		state[ruleID] = stateVisited
-	}
-
-	for i := range g.Rules {
-		dfs(i)
 	}
 }
