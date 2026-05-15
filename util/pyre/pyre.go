ff5fc8dc39b2cb774c905499f59c4dc74bbe994c
apalala <apalala@gmail.com>
2026-05-15 08:41:47 -0400

    [pyre] new implementations with pcre2

--- util/pyre/pyre.go
+++ util/pyre/pyre.go
@@ -1,11 +1,11 @@
 package pyre
 
 func Compile(pattern string) (Pattern, error) {
-	return NewRegexp2Pattern(pattern)
+	return NewPCRE2CgoPattern(pattern)
 }
 
 func MustCompile(pattern string) Pattern {
-	p, err := NewRegexp2Pattern(pattern)
+	p, err := NewPCRE2CgoPattern(pattern)
 	if err != nil {
 		panic(err)
 	}
