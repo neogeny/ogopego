9f61808962f6ff9ac74c29e0e14604e212a942d2
apalala <apalala@gmail.com>
2026-05-23 12:55:37 -0400

    [fragments]

--- fragments/tag.sh
+++ fragments/tag.sh
@@ -1,2 +1,2 @@
-# GOPROXY=https://proxy.golang.org go list -m github.com/neogeny/ogopego@v0.1.0
-GOPROXY=https://proxy.golang.org go list -m github.com/neogeny/ogopego
+# GOPROXY=https://proxy.golang.org go list -mod=mod -m github.com/neogeny/ogopego@v0.1.0
+GOPROXY=https://proxy.golang.org go list -mod=mod -m github.com/neogeny/ogopego
