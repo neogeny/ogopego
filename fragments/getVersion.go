c05d9df7a65c52a3f08c64f502ffb30f39927af5
apalala <apalala@gmail.com>
2026-05-22 20:28:31 -0400

    [fragments] getVersion

---
+++ fragments/getVersion.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"runtime/debug"
+)
+
+func getVersion() string {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return "unknown"
+	}
+
+	// 1. If installed via 'go install github.com/user/repo@v0.1.0'
+	if info.Main.Version != "" && info.Main.Version != "(devel)" {
+		return info.Main.Version
+	}
+
+	// 2. If built locally from source (e.g., 'go build'), dig into the VCS settings
+	var revision string
+	var dirty bool
+
+	for _, setting := range info.Settings {
+		switch setting.Key {
+		case "vcs.revision":
+			revision = setting.Value
+		case "vcs.modified":
+			dirty = setting.Value == "true"
+		}
+	}
+
+	if revision != "" {
+		shortRev := revision
+		if len(shortRev) > 7 {
+			shortRev = shortRev[:7]
+		}
+		if dirty {
+			return fmt.Sprintf("devel-%s-dirty", shortRev)
+		}
+		return fmt.Sprintf("devel-%s", shortRev)
+	}
+
+	return "unknown"
+}
+
+func main() {
+	fmt.Printf("OgoPego version: %s\n", getVersion())
+}
